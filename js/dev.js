b9dfb43d5be3cce8b5441e3e7b6058d9b1c9c750
shaker11batch <saenterprise67811@gmail.com>
2025-03-02 19:53:26 +0600

    dev created

--- js/dev.js
+++ js/dev.js
@@ -1,1 +1,9 @@
 // alert('amr sonir bangla')
+const allCompletedBtn =document.querySelectorAll(".completed-btn")
+// console.log(allCompletedBtn)
+for(const handleCompletedBtn of allCompletedBtn){
+    // console.log(handleCompletedBtn)
+    handleCompletedBtn.addEventListener('click', ()=>{
+        console.log('object')
+    })
+}
