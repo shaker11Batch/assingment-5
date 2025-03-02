ad46d5bf0fff140c1acc528960d6b3bcb8b05f4f
shaker11batch <saenterprise67811@gmail.com>
2025-03-02 20:06:11 +0600

    5th

--- js/dev.js
+++ js/dev.js
@@ -4,6 +4,14 @@ const allCompletedBtn =document.querySelectorAll(".completed-btn")
 for(const handleCompletedBtn of allCompletedBtn){
     // console.log(handleCompletedBtn)
     handleCompletedBtn.addEventListener('click', ()=>{
-        console.log('object')
+        // console.log('object')
+        const dicriseId = parseInt(document.getElementById('dicrise').innerText)
+        console.log(dicriseId)
+        const newValue =Math.max(0, dicriseId -1);
+        document.getElementById('dicrise').innerText= newValue;
+        const incriseId = parseInt(document.getElementById('incrise').innerText)
+        console.log(dicriseId)
+        const newValueCount =Math.max(0, incriseId +1);
+        document.getElementById('incrise').innerText= newValueCount;
     })
 }
