ec3a1d4d179e4cf4d34d9054498f3a89116fc768
shaker11batch <saenterprise67811@gmail.com>
2025-03-02 20:49:48 +0600

    final

--- js/dev.js
+++ js/dev.js
@@ -2,6 +2,7 @@
 const allCompletedBtn =document.querySelectorAll(".completed-btn")
 // console.log(allCompletedBtn)
 for(const handleCompletedBtn of allCompletedBtn){
+    const date = new Date()
     // console.log(handleCompletedBtn)
     handleCompletedBtn.addEventListener('click', ()=>{
         // console.log('object')
@@ -13,5 +14,23 @@ for(const handleCompletedBtn of allCompletedBtn){
         console.log(dicriseId)
         const newValueCount =Math.max(0, incriseId +1);
         document.getElementById('incrise').innerText= newValueCount;
+        const container =document.getElementById('history-container')
+        document.getElementById('history-container').style.display ='block'
+        const div = document.createElement('div')
+        div.classList.add('mt-5')
+        
+        div.innerHTML=`
+        <p class="text-canter">You hove complete the task</p>
+        <p class="text-canter">${date}</p>
+        `
+        container.appendChild(div)
+
     })
 }
+
+document.getElementById('history-clear').addEventListener('click',()=>{
+    document.getElementById('history-container').style.display='none'
+})
+document.getElementById('move-page').addEventListener('click', ()=>{
+    window.location.href ='question.html'
+})
